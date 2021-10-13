b3281142e0bda335984892ebe096285d8e60115c
Nadia Sanabria <ing.nsanabriab16@gmail.com>
2021-10-12 20:53:22 -0500

    Creacion proyecto prueba Spring Boot

---
+++ Backend/src/main/java/Ciclo3_Reto3_Ejemplo/Ciclo3_Reto3_Ejemplo/Ciclo3Reto3EjemploApplication.java
@@ -0,0 +1,13 @@
+package Ciclo3_Reto3_Ejemplo.Ciclo3_Reto3_Ejemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ciclo3Reto3EjemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ciclo3Reto3EjemploApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/Ciclo3_Reto3_Ejemplo/Ciclo3_Reto3_Ejemplo/Ciclo3Reto3EjemploApplicationTests.java
@@ -0,0 +1,13 @@
+package Ciclo3_Reto3_Ejemplo.Ciclo3_Reto3_Ejemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ciclo3Reto3EjemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
